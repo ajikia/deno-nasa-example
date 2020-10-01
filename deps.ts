94355f06fe0d03425d524a60b55eb545e575f0c0
Ajikia <kokjikia@gmail.com>
2020-10-01 17:28:26 +0400

    initial commit

---
+++ deps.ts
@@ -0,0 +1,11 @@
+// Standard
+export * as log from "https://deno.land/std/log/mod.ts";
+export { join } from "https://deno.land/std/path/mod.ts";
+export { parse } from "https://deno.land/std/encoding/csv.ts";
+export { BufReader } from "https://deno.land/std/io/bufio.ts";
+
+// Third Party
+export { Application, send } from "https://deno.land/x/oak@v6.2.0/mod.ts";
+export { Router } from "https://deno.land/x/oak@v6.2.0/mod.ts";
+export * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+export { pick } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
